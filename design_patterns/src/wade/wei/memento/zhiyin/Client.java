a1a21bde9d8f12c4aded7474dae68757ddd498ae
wade <745696658@qq.com>
2019-08-30 15:06:57 +0800

    备忘录模式

--- design_patterns/src/wade/wei/memento/zhiyin/Client.java
+++ design_patterns/src/wade/wei/memento/zhiyin/Client.java
@@ -7,9 +7,9 @@ public class Client {
     public static void main(String[] args) {
         Document dd = new Document("dd");
         Editor editor = new Editor(dd);
-        editor.append("sssss");
-        editor.append("ffffff");
-        editor.append("cccccccc");
+        editor.append("one；");
+        editor.append("two；");
+        editor.append("three；");
 
         editor.delete();
 
